1e2b09d992800d526071a04a79c7064de626d3a2
r3dsd <dogsheepduck@gmail.com>
2024-12-01 16:34:50 +0900

    nav bug fix

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem "jekyll", ">= 3.9", "< 5.0"
 # If you have any plugins, put them here!
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.12"
-  # gem "jekyll-r3-blog", "0.1.0", :path => '/home/[name]/utils/jekyll-r3-blog'
+  # gem "jekyll-r3-blog", "0.1.0", :path => '/home/yehwan/utils/jekyll-r3-blog'
 end
 
 gem "jekyll-sitemap", "~> 1.3"
--- assets/js/post.js
+++ assets/js/post.js
@@ -112,13 +112,6 @@ document.addEventListener('DOMContentLoaded', function(){
         });
     }
 
-    // Move to Comment
-    document.getElementById('comments-counter').addEventListener('click', function(){
-        document.getElementById("giscus").scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-
     // Code highlighter
     if (currentTheme === 'dark'){
         // Disable highlighter default color theme
